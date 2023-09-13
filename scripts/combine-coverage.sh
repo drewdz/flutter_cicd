958f5d2c7e9bcf03ec3e6c7a67219f9cd9eff597
Andrew D'Alton <adalton@productiveedge.com>
2023-09-13 14:06:41 -0500

    more help 2

--- scripts/combine-coverage.sh
+++ scripts/combine-coverage.sh
@@ -5,8 +5,10 @@ while read FILENAME; do
   LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$PROJECT_ROOT_PATH/coverage/$FILENAME\""
 done < <( ls "$1/coverage/" )
 
+echo "- ready to combine files"
 echo "${LCOV_INPUT_FILES}"
 
+echo "- combining files"
 eval lcov "${LCOV_INPUT_FILES}" -o $PROJECT_ROOT_PATH/coverage_report/combined_lcov.info
 
 lcov --remove $PROJECT_ROOT_PATH/coverage_report/combined_lcov.info \
